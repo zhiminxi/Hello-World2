d48494a7e71e2eca1bb8a2e213049b471c9f5257
zhimin.xi <zhimin.xi@pcc.edu>
2018-05-08 11:35:06 -0700

    add main method that prints hello world

--- src/edu/pcc/cis/helloworld2/helloworld2.java
+++ src/edu/pcc/cis/helloworld2/helloworld2.java
@@ -5,5 +5,9 @@ package edu.pcc.cis.helloworld2;
  * @version 2018.5.7
  */
 public class helloworld2 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!!");
 
     }
+}
