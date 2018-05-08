c228eade2726d4be7d8c26359d0d58485eaa492d
zhimin.xi <zhimin.xi@pcc.edu>
2018-05-08 11:36:28 -0700

    print date

--- src/edu/pcc/cis/helloworld2/helloworld2.java
+++ src/edu/pcc/cis/helloworld2/helloworld2.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.helloworld2;
 
+import java.util.Date;
+
 /**
  * @author Zhimin Xi
  * @version 2018.5.7
@@ -8,6 +10,7 @@ public class helloworld2 {
     public static void main(String[] args) {
 
         System.out.println("Hello World!!");
+        System.out.println(new Date());
 
     }
 }
