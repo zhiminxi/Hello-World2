457943f521f978420074796af95c080cc6e4624c
zhimin.xi <zhimin.xi@pcc.edu>
2018-05-08 11:32:13 -0700

    ready to coding

--- src/edu/pcc/cis/helloworld2/helloworld2.java
+++ src/edu/pcc/cis/helloworld2/helloworld2.java
@@ -1,7 +1,9 @@
 package edu.pcc.cis.helloworld2;
 
+/**
+ * @author Zhimin Xi
+ * @version 2018.5.7
+ */
 public class helloworld2 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!!");
+
     }
-}
